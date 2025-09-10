c29c9667c3a0e1e04490c8d9ed8c20235bbcdf6d
maodong7 <2925540856@qq.com>
2025-09-10 16:42:50 +0800

    Add worksheet2 contents

---
+++ worksheet2/my-nest-app/src/dummydata/articles.ts
@@ -0,0 +1,5 @@
+export const ARTICLES = [
+  { id: 1, title: "Article 1", content: "Content of article 1" },
+  { id: 2, title: "Article 2", content: "Content of article 2" },
+  { id: 3, title: "Article 3", content: "Content of article 3" },
+];
